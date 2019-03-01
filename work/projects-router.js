599fd1a45814618c287c9309c10a560d8140870b
Samsoucy <newf930@outlook.com>
2019-03-01 17:46:11 -0500

    fixed get route so that it shows project info and actions for that project. Also started stretch and added all the CRUD functions for projects

--- work/projects-router.js
+++ work/projects-router.js
@@ -8,24 +8,37 @@ const db = knex(knexConfig.development)
 
 
 
-// ******all actions for a project with the specified id*****/
+//*************returns an array of all projects*************/
 
-router.get('/:id/actions', (req, res) => {
-    const { id } = req.params
-    db('actions')
-    .where("project_id", id)
-        .then(names => {
-            if (names.length) {
-                res.json(names)
-            } else {
-                res.status(400).json({ message: "This project does not have any actions" })
-            }
+router.get('/', (req, res) => {
+    db('projects')
+    .then(projects => {
+      res.status(200).json(projects)
     })
-    .catch(() => {
-        res.status(500).json({ message: "Could not find actions in this project" })
+    .catch(error => {
+      res.status(500).json(error)
     })
 });
 
+
+
+// ******all actions for a project with the specified id*****/
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    db("projects")
+        .where({ id })
+        .then(project => {
+    db('actions')
+        .where({ project_id : id })
+        .then(action => {
+            res.status(200).json({project, action})
+        })
+        .catch(err => res.status(500).json(err))
+    })
+    .catch(err => res.status(500).json(err))
+    
+})
 
 
 
@@ -48,6 +61,48 @@ router.post("/", (req, res) => {
         })
 });
 
+//***********update a project****************/
+
+router.put('/:id', (req, res) => {
+    db('projects')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(response => {
+      if(response > 0) {
+        db('projects')
+        .where({ id: req.params.id })
+        .first()
+        .then(response => {
+          res.status(200).json(response)
+        })
+      } else {
+        res.status(404).json({ message: 'project not found' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+});
+
+//*************delete a project**************/
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    db('projects')
+    db('projects')
+    .where({ id })
+    .del()
+    .then(response => {
+      if(response > 0) {
+        res.status(204).end()
+      } else {
+        res.status(404).json({ message: 'That project could not be found' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  });
 
 
 
