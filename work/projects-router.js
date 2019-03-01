a159932b3a14398f1817a00d987499b608884c32
Samsoucy <newf930@outlook.com>
2019-03-01 13:48:20 -0500

    added a get to retrieve actions for a specific project with id

--- work/projects-router.js
+++ work/projects-router.js
@@ -7,6 +7,28 @@ const knexConfig = require('../knexfile.js')
 const db = knex(knexConfig.development)
 
 
+
+// ******all actions for a project with the specified id*****/
+
+router.get('/:id/actions', (req, res) => {
+    const { id } = req.params
+    db('actions')
+    .where("project_id", id)
+        .then(names => {
+            if (names.length) {
+                res.json(names)
+            } else {
+                res.status(400).json({ message: "This project does not have any actions" })
+            }
+    })
+    .catch(() => {
+        res.status(500).json({ message: "Could not find actions in this project" })
+    })
+});
+
+
+
+
 //************adds a new project************/
 
 router.post("/", (req, res) => {
